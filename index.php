f7121ab5df7c38b329a6da2f8f75c56c98b0557a
binhle2504 <accfreefire2504@gmail.com>
2023-10-21 01:03:00 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+//  include_once "./app/controllers/ProductController.php";
+//  include_once "./app/models/Product.php";
+//  include_once "./app/controllers/CustomerController.php";
+//  include_once "./app/models/Customer.php";
+session_start();
+require "env.php";
+require "vendor/autoload.php";
+require "common/route.php";
+// use App\Controllers\ProductController;
+// use App\Controllers\CustomerController;
+// use App\Models\Customer;
+// use App\Models\Product;
+// $productController= new ProductController();
+// $CustomerController = new CustomerController();
+// $product = new Product();
+// $customer = new Customer();
+// // composer init để: khơir tạo 
+// // composer dump-autoload de chay
+
+
+
+
+
+?>
